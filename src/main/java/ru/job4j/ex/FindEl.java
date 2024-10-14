15515e1d158a2331e372ca5c1fb5e8494a154c53
masha <musha_skorpion@mail.ru>
2024-10-15 00:29:43 +0500

    0.4. Пользовательские исключения. [#219367 #516392]

--- src/main/java/ru/job4j/ex/FindEl.java
+++ src/main/java/ru/job4j/ex/FindEl.java
@@ -3,10 +3,10 @@ package ru.job4j.ex;
 public class FindEl extends Exception {
     public static int indexOf(String[] value, String key) throws ElementNotFoundException {
         int result = -1;
-        /* цикл fori, поскольку нам надо найти индекс искомого элемента в массиве */
         for (int i = 0; i < value.length; i++) {
             if (key.equals(value[i])) {
                 result = i;
+                break;
             }
         }
         if (result == -1)  {
