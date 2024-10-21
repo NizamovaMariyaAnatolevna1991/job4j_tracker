000cc2eb3bdd0b69ea686f34232c87d6ef59e9ff
masha <musha_skorpion@mail.ru>
2024-10-21 15:30:20 +0500

    0.5. Иерархия исключений и множественный catch. [#219368]

---
+++ src/main/java/ru/job4j/ex/UserStore.java
@@ -0,0 +1,36 @@
+package ru.job4j.ex;
+
+public class UserStore {
+    public static User findUser(User[] users, String login) throws UserNotFoundException {
+        for (User user : users) {
+            if (user.getUsername().equals(login)) {
+                return user;
+            }
+        }
+        throw new UserNotFoundException("User not found: " + login);
+    }
+
+    public static boolean validate(User user) throws UserInvalidException {
+        if (user == null || !user.isValid() || user.getUsername().length() < 3) {
+            throw new UserInvalidException("User is invalid or username is too short");
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        User[] users = {
+                new User("Petr Arsentev", true)
+        };
+
+        try {
+            User user = findUser(users, "Pe");
+            if (validate(user)) {
+                System.out.println("This user has an access");
+            }
+        } catch (UserInvalidException e) {
+            System.out.println(e.getMessage());
+        } catch (UserNotFoundException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
