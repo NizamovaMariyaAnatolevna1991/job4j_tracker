c41bb78496c5a900b4eb04b6294562ae4aa8df2b
masha <musha_skorpion@mail.ru>
2025-01-02 09:06:38 +0500

    Map, HashMap [#212652 #526759]

--- src/main/java/ru/job4j/collection/UsageMap.java
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -6,6 +6,9 @@ public class UsageMap {
     public static void main(String[] args) {
         HashMap<String, String> map = new HashMap<>();
         map.put("nizamova@mail.ru", "Низамова Мария Анатольевна");
+        map.put("nizamov@mail.ru","Низамов Тимур Ильдарович");
+        map.put("nizamov@mail.ru","Низамов Григорий Тимурович");
+        map.put("saharova@maol.ru", "Сахарова Екатерина Александровна");
         for (String key : map.keySet()) {
             String value = map.get(key);
             System.out.println(key + " = " + value);
