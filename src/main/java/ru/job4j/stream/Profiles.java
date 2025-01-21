599bc7246be5590496192415821a9ed8cb31e815
masha <musha_skorpion@mail.ru>
2025-01-21 23:19:19 +0500

    6. Список адресов [#110062 #529127]

--- src/main/java/ru/job4j/stream/Profiles.java
+++ src/main/java/ru/job4j/stream/Profiles.java
@@ -10,7 +10,6 @@ public class Profiles {
     public static List<Address> collect(List<Profile> profiles) {
         return profiles.stream()
                 .map(Profile::getAddress)
-                .distinct()
                 .collect(Collectors.toList());
     }
 
